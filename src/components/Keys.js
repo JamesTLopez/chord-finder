58df8107b5f6f11cf69369da5fa26b0ee1ad3bb9
James <jtlopez919@gmail.com>
2020-08-30 21:35:07 -0400

    Added keyboard guide

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -11,7 +11,6 @@ import { UPDATE_CHORD, PLAY_NOTE ,KEY_UP , KEY_DOWN} from '../store/actions/type
 function Keys(props) {
     
     let octaveController = props.octaveM;
-
     let [pressedKey] = useState(props.pressedKey);
 
     
@@ -65,44 +64,86 @@ function Keys(props) {
                 <div className="Keys">
                     <div className="white-key">
                         <img className="C" id={pressedKey[0]?"pressedKey" : "note"} src={White} alt="C" onClick={()=>playN("C" + octaveController[0])}></img>
+                        <label>
+                            A
+                        </label>
                     </div>
                     <div className="black-key">
                         <img className="Db" id={pressedKey[1]?"pressedKey" : "note"} src={Black} alt="Db" onClick={()=>playN('Db'+ octaveController[0])}></img>
+                        <label>
+                            W
+                        </label>
                     </div>
                     <div className="white-key">
                         <img className="D" id={pressedKey[2]?"pressedKey" : "note"} src={White} alt="D" onClick={()=>playN("D" + octaveController[0])}></img>
+                        <label>
+                            S
+                        </label>
                     </div>
                     <div className="black-key">
                         <img className="Eb" id={pressedKey[3]?"pressedKey" : "note"} src={Black} alt="Eb" onClick={()=>playN('Eb'+ octaveController[0])}></img>
+                        <label>
+                            E
+                        </label>
                     </div>
                     <div  className="white-key">
                         <img className="E" id={pressedKey[4]?"pressedKey" : "note"} src={White} alt="E" onClick={()=>playN("E" + octaveController[0])}></img>
+                        <label>
+                            D
+                        </label>
                     </div>
                     <div className="white-key">
                         <img className="F" id={pressedKey[5]?"pressedKey" : "note"} src={White} alt="F" onClick={()=>playN("F" + octaveController[0])}></img>
+                        <label>
+                            F
+                        </label>
+                        <label>
+                            F
+                        </label>
                     </div>
                     <div className="black-key">
                         <img className="Gb" id={pressedKey[6]?"pressedKey" : "note"} src={Black} alt="Gb" onClick={()=>playN('Gb'+ octaveController[0])}></img>
+                        <label>
+                            T
+                        </label>
                     </div>
                     <div className="white-key">
                         <img className="G" id={pressedKey[7]?"pressedKey" : "note"} src={White} alt="G" onClick={()=>playN("G" + octaveController[0])}></img>
+                        <label>
+                            G
+                        </label>
                     </div>
                     <div className="black-key">
                         <img className="Ab" id={pressedKey[8]?"pressedKey" : "note"} src={Black} alt="Ab" onClick={()=>playN('Ab'+ octaveController[0])}></img>
+                        <label>
+                            Y
+                        </label>
                     </div>
                     <div className="white-key">
                         <img className="A" id={pressedKey[9]?"pressedKey" : "note"} src={White} alt="A" onClick={()=>playN("A" + octaveController[0])}></img>
+                        <label>
+                            H
+                        </label>
                     </div>
                     <div className="black-key">
                         <img className="Bb" id={pressedKey[10]?"pressedKey" : "note"} src={Black} alt="Bb" onClick={()=>playN('Bb'+ octaveController[0])}></img>
+                        <label>
+                            U
+                        </label>
                     </div>
                     <div className="white-key">
                         <img className="B" id={pressedKey[11]?"pressedKey" : "note"} src={White} alt="B" onClick={()=>playN("B" + octaveController[0])}></img>
+                        <label>
+                            J
+                        </label>
                     </div>
                 </div>
                 <div className="Keys">
                     <div className="white-key">
                         <img id={pressedKey[12]?"pressedKey" : "note"} src={White} alt="C" onClick={()=>playN("C" + octaveController[2])}></img>
+                        <label>
+                            K
+                        </label>
                     </div>
                     <div className="black-key">
                         <img id={pressedKey[13]?"pressedKey" : "note"} src={Black} alt="Db" onClick={()=>playN('Db'+ octaveController[2])}></img>
